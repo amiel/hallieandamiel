cdbdbe3105ca3f1fe3ca64e58f74e919eeaa8f5f
Adrian Pike <adrian@pikeapps.com>
2011-08-02 10:52:59 -0700

    RAILS THREE POINT ONE FOR GREAT JUSTICE AND MARRIAGE!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hallieandamiel::Application.config.secret_token = '46eba31c5db88a90031339bfcd6b0b190235659ef5068d25c38cd60aeb8a9e8af7234909fe8c6624faeae9f6433887abf287932b1dc11a8eeb6a635a2ceac524'

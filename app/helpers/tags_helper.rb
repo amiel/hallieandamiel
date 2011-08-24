ec81d9f4b5b46862c21d5a45afe2060fa9caea24
Amiel Martin <amiel@carnesmedia.com>
2011-08-24 11:52:29 -0700

    split up tags by tag category

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,11 @@
 module TagsHelper
+  def title_for_category(category)
+    # TODO: I18n
+    case category
+    when 'user'
+      'By Photographer'
+    else
+      'By Category'
+    end
+  end
 end

505a09ee7891fea70ab0a06b566c8e443acde529
Amiel Martin <amiel.martin@gmail.com>
2011-08-26 15:23:27 -0700

    Add route for duplicates

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Hallieandamiel::Application.routes.draw do
   resources :tags
-  
+
   resources :photos do |pho| #sho
     resources :tags
     collection do
       get :unapproved
+      get :duplicates
     end
   end
-  
+
   match "/categories", :to => 'tags#index' # Because tags is a scary word.
   match "/albums", to: 'tags#index', as: 'albums' # Because tags is a scary word.
-  
+
   match "/thanks", to: 'photos#thanks', as: 'thank_you' # Because shut up REST is why
-  
+
   root :to => 'tags#index'
 end
